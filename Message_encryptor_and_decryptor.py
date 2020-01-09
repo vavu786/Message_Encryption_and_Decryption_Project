aff86fac7802d1ca3ff130ee130cff8d8fa66bd3
vavu786 <55862742+vavu786@users.noreply.github.com>
2020-01-09 13:37:20 -0600

    Update Message_encryptor_and_decryptor.py
    Working on making the message numbers into one string

--- Message_encryptor_and_decryptor.py
+++ Message_encryptor_and_decryptor.py
@@ -33,6 +33,9 @@ if conditionToEncode.lower() == "d":
     counter = 0
     rowNumMessage = int(messageSize / messageSplit)
     messageMatrix = np.empty([rowNumMessage, messageSplit], dtype='int')
+
+    messageValues = input("Enter the numbers of the message, each separated by a space: ")
+    '''
     for i in range(rowNumMessage):
         for j in range(messageSplit):
             counter = counter + 1
@@ -41,16 +44,14 @@ if conditionToEncode.lower() == "d":
                 messageMatrix[i][j] = valueM
             if counter > messageSize:
                 messageMatrix[i][j] = 0
+    '''
     # End up with a messageMatrix with numbers of the message
 
 if conditionToEncode.lower() == "e":
     # Defines the message matrix (characters)
     message = input("Enter the message to encode: ")
     messageSize = len(message)
-    # while isPrime(messageSize) == True:
-    # messageSize = int(input("The encoding process cannot be done with a prime number of characters(enter spaces after the message to make the number of characters not prime): "))
-    messageSplit = int(input(
-        "How many rows does the square encoding matrix have?(it should be a factor of the size of the message): "))
+    messageSplit = int(input("How many rows does the square encoding matrix have?(it should be a factor of the size of the message): "))
     # This while loop just adds spaces to the end of the message to make the message and the matrix compatible
     while messageSize % messageSplit != 0:
         message = message + " "
@@ -65,19 +66,6 @@ if conditionToEncode.lower() == "e":
         # Otherwise, make the number the coresponding value of the alphabet(a = 1, b = 2, c = 3...)
         else:
             messageNumbers[i] = (ord(char) - 96)
-    # This while loop makes sure that the encoding matrix and the subsets of the message are compatible for multiplication
-    while messageSize % messageSplit != 0:
-        condOne = input(
-            "The encoding process cannot occur since the size of the encoding matrix was not a factor of the message size. Please either change the encoding matrix dimension(enter an E) or change the message matrix dimension(enter an M): ")
-        while condOne.lower() != "e" and condOne.lower() != "m":
-            cond1 = input("Please enter either an E or an M: ")
-        if condOne.lower() == "e":
-            messageSplit = int(input(
-                "How many rows should the square encoding matrix have?(it should be a factor of the size of the message): "))
-        if condOne.lower() == "m":
-            messageSize = int(input("How many numbers are in the message?(cannot be prime): "))
-            while isPrime(messageSize) == True:
-                messageSize = int(input("The encoding process cannot be done with a prime number: "))
     rowNumMessage = int(messageSize / messageSplit)
     messageMatrix = messageNumbers.reshape(rowNumMessage, messageSplit)
 
@@ -92,16 +80,21 @@ for i in range(dimofEncMatix):
 
 # Actual code for encrypting and decrypting
 if conditionToEncode.lower() == "e":
-    print("You will be encrypting the numbers")
-    print(messageMatrix.reshape(messageSize))
-    encodedMessage = np.dot(messageMatrix, encodingMatrix).reshape(messageSize)
+    conditionToDecrypt = 0
+    meanMatrix = np.round(np.mean(encodingMatrix))
+    if meanMatrix % 2 == 0:
+        encodedMessage = np.dot(messageMatrix, encodingMatrix**2).reshape(messageSize)
+        conditionToDecrypt = 2
+    else:
+        encodedMessage = np.dot(messageMatrix, encodingMatrix**3).reshape(messageSize)
+        conditionToDecrypt = 3
     print("The encoded numbers of the message are")
     print(encodedMessage)
 if conditionToEncode.lower() == "d":
-    print("You will be decrypting the numbers")
-    print(messageMatrix.reshape(messageSize))
-    decodedMessage = np.dot(messageMatrix, np.linalg.inv(encodingMatrix)).reshape(messageSize)
-    print(np.linalg.inv(encodingMatrix))
+    if conditionToDecrypt == 2:
+        decodedMessage = np.dot(messageMatrix, np.linalg.inv(encodingMatrix**2)).reshape(messageSize)
+    if conditionToDecrypt == 3:
+        decodedMessage = np.dot(messageMatrix, np.linalg.inv(encodingMatrix**3)).reshape(messageSize)
     print("The decoded numbers of the message are")
     decodedMessage = decodedMessage.astype(int)
     print(decodedMessage)

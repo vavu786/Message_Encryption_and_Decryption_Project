0f89ddf27f2aa0693fde58b0de8e0489a7da38ec
vavu786 <hamzamian.patwa@gmail.com>
2021-01-03 10:22:05 -0600

    Added decrypt() and encrypt() functions to clean the code Replaced interactive input with an input file

--- Message_encryptor_and_decryptor.py
+++ Message_encryptor_and_decryptor.py
@@ -1,54 +1,21 @@
+"""
+input.txt is formatted as follows:
+Decryption:
+    d or D (char)
+    Dimension of encoding matrix (int)
+    Values of encoding matrix (space-separated ints)
+    Number of letters in message (int)
+    Values of the encrypted message (space-separated ints)
+Encryption:
+    e or E (char)
+    Dimension of encoding matrix (int)
+    Values of encoding matrix (space-separated ints)
+    Message (string)
+"""
+
 import numpy as np
-import itertools
-import math
 
 np.set_printoptions(suppress=True)
-
-# Makes sure that the user chooses either E or D
-conditionToEncode = input("Would you like to encrypt(enter an E) or decrypt(enter a D) a message? ")
-while conditionToEncode.lower() != "e" and conditionToEncode.lower() != "d":
-    conditionToEncode = input("Please enter either a letter E or D: ")
-messageSplit = int(input("How many rows does the square encoding matrix have?: "))
-
-# Makes the values of the matrix equal to the user input
-encodingValues = input("Enter the values of the encoding matrix in one line: ")
-encodingMatrix = np.asarray(encodingValues.split(" "), dtype='int').reshape(messageSplit, messageSplit)
-
-if conditionToEncode.lower() == "d":
-    # Defines the message matrix
-    messageSize = int(input("How many numbers are in the message?: "))
-    messageSizeExtra = messageSize
-    # This while loop just adds spaces to the end of the message to make the message and the matrix compatible
-    while messageSizeExtra % messageSplit != 0:
-        messageSizeExtra = messageSizeExtra + 1
-    # Makes the values of the matrix equal to the user input
-    counter = 0
-    rowNumMessage = int(messageSizeExtra / messageSplit)
-    # messageMatrix = np.empty([rowNumMessage, messageSplit], dtype='int')
-    messageValues = input("Enter the numbers of the message, each separated by a space: ")
-    messageMatrix = np.asarray(messageValues.split(" "), dtype='int').reshape(rowNumMessage, messageSplit)
-    # End up with a messageMatrix with numbers of the message
-
-if conditionToEncode.lower() == "e":
-    # Defines the message matrix (characters)
-    message = input("Enter the message to encode: ")
-    messageSize = len(message)
-    # This while loop just adds spaces to the end of the message to make the message and the matrix compatible
-    while messageSize % messageSplit != 0:
-        message = message + " "
-        messageSize = len(message)
-    # messageNumbers is the array of the characters of the message converted to numbers
-    messageNumbers = np.empty([messageSize], dtype='int')
-    # i is the indexing variable and char is the python loop variable, enumerate is just fancy syntax for that
-    for i, char in enumerate(message):
-        # If the character is a space, make the number 0 instead of 32
-        if char == " ":
-            messageNumbers[i] = 0
-        # Otherwise, make the number the corresponding value of the alphabet(a = 1, b = 2, c = 3...)
-        else:
-            messageNumbers[i] = ((ord(char) - 96) + (encodingMatrix[0][0] % 26))
-    rowNumMessage = int(messageSize / messageSplit)
-    messageMatrix = messageNumbers.reshape(rowNumMessage, messageSplit)
 
 
 def factor(matrix):
@@ -59,37 +26,96 @@ def factor(matrix):
         return 3
 
 
-# Actual code for encrypting and decrypting
-encodedMessage = None
+def encrypt(originalMessage, encodingMatrix):
+    # This while loop just adds spaces to the end of the message to make the message and the matrix compatible
+    while len(originalMessage) % len(encodingMatrix) != 0:
+        originalMessage = originalMessage + " "
 
-if conditionToEncode.lower() == "e":
+    # originalMessageNumbers is the flat array of the characters of the message converted to numbers
+    originalMessageNumbers = np.empty([len(originalMessage)], dtype='int')
+
+    # i is the indexing variable and char is the python loop variable, enumerate is just fancy syntax for that
+    for i, char in enumerate(originalMessage):
+        # If the character is a space, make the number 0 instead of 32
+        if char == " ":
+            originalMessageNumbers[i] = 0
+        # Otherwise, make the number the corresponding value of the alphabet(a = 1, b = 2, c = 3...)
+        else:
+            originalMessageNumbers[i] = ((ord(char) - 96) + (encodingMatrix[0][0] % 26))
+
+    # originalMessageMatrix is the shaped matrix of the encoded (right now partially encoded) message numbers
+    originalMessageMatrix = originalMessageNumbers.reshape(int(len(originalMessage) / len(encodingMatrix)), len(encodingMatrix))
+
+    # Applies the actual encryption algorithm
     if factor(encodingMatrix) == 2:
-        encodedMessage = np.matmul(messageMatrix, np.linalg.matrix_power(encodingMatrix, 2)).reshape(messageSize)
+        encodedMessage = np.matmul(originalMessageMatrix, np.linalg.matrix_power(encodingMatrix, 2)).reshape(len(originalMessage))
     else:
-        encodedMessage = np.matmul(messageMatrix, np.linalg.matrix_power(encodingMatrix, 3)).reshape(messageSize)
-    print("The encoded numbers of the message are")
-    print(encodedMessage)
+        encodedMessage = np.matmul(originalMessageMatrix, np.linalg.matrix_power(encodingMatrix, 3)).reshape(len(originalMessage))
 
-decodedMessage = None
-invEncodingMatrix = None
+    # Converts encrypted numbers into formatted string that decrypt() can take in
+    encodedMessageString = ""
+    for encryptedNumber in encodedMessage:
+        encodedMessageString = encodedMessageString + str(encryptedNumber) + " "
+    return encodedMessageString.strip()
 
-if conditionToEncode.lower() == "d":
+
+def decrypt(messageNumbers, encodingMatrix):
+    # Formats messageNumbers to get messageNumbers of type list instead if string
+    messageNumbers = messageNumbers.split(" ")
+
+    # encryptedMessageMatrix is a compatible, numpy array of the message numbers
+    encryptedMessageMatrix = np.asarray(messageNumbers, dtype='int').reshape(int(len(messageNumbers) / len(encodingMatrix)), len(encodingMatrix))
+
+    # Actual decryption process
     if factor(encodingMatrix) == 2:
         invEncodingMatrix = np.linalg.inv(np.linalg.matrix_power(encodingMatrix, 2))
-        decodedMessage = np.around(np.matmul(messageMatrix, invEncodingMatrix).reshape(messageSize))
+        decodedMessage = np.around(np.matmul(encryptedMessageMatrix, invEncodingMatrix).reshape(len(messageNumbers)))
     else:
         invEncodingMatrix = np.linalg.inv(np.linalg.matrix_power(encodingMatrix, 3))
-        decodedMessage = np.around(np.matmul(messageMatrix, invEncodingMatrix).reshape(messageSize))
-    print("The decoded numbers of the message are")
-    decodedMessage = decodedMessage.astype(int)
-    for i in range(messageSize):
+        decodedMessage = np.around(np.matmul(encryptedMessageMatrix, invEncodingMatrix).reshape(len(messageNumbers)))
+
+    for i in range(len(messageNumbers)):
         if decodedMessage[i] != 0:
             decodedMessage[i] = decodedMessage[i] - (encodingMatrix[0][0] % 26)
 
-    print(decodedMessage)
-    print("The message says: ")
+    # Converts float array to int array so the numbers can be converted to letters (ex: 6.0 -> 6 -> 'f')
+    decodedMessage = decodedMessage.astype(int)
+
+    # Gets printable string of the decrypted message
+    decodedMessageString = ""
     for number in decodedMessage:
         if number != 0:
-            print(chr(number + 96), end='')
-        if number <= 0:
-            print(" ", end='')
+            decodedMessageString = decodedMessageString + chr(number + 96)
+        if number == 0:
+            decodedMessageString = decodedMessageString + " "
+
+    return decodedMessageString.strip()
+
+
+def main():
+    # User chooses "e" or "d"
+    encodeOrDecode = input()
+
+    # Dimension of square encoding matrix
+    dimEncodingMatrix = int(input())
+
+    # input() is the values of the encoding matrix in one line
+    encodingMatrix = np.asarray(input().split(" "), dtype='int').reshape(dimEncodingMatrix, dimEncodingMatrix)
+
+    # Setup to decode: gets encrypted message and calls decrypt()
+    if encodeOrDecode.lower() == "d":
+        # Encrypted message numbers, each separated by a space
+        messageValues = input()
+        decodedMessage = decrypt(messageValues, encodingMatrix)
+        print(decodedMessage)
+
+    # Setup to encode: gets original message and calls encrypt()
+    if encodeOrDecode.lower() == "e":
+        # Message to encode
+        message = input()
+        encodedMessage = encrypt(message, encodingMatrix)
+        print(encodedMessage)
+
+
+if __name__ == "__main__":
+    main()
